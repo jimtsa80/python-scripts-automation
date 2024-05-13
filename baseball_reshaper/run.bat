45dad9498bd0f6fcf37750aa882ad9534d2b0d98
Dimitris Tsarouhas <dtsarouhas@omilia.com>
2024-05-13 23:46:59 +0300

    run.bat

---
+++ baseball_reshaper/run.bat
@@ -0,0 +1,26 @@
+@echo off
+
+REM Run Script 1: Creates the Inning column
+echo Running Script 1...
+python inning_creator.py %1
+
+REM Run Script 2: Makes duration 1 in each line
+echo Running Script 2...
+python reshaper.py %1
+
+REM Run Script 3: Concatanates Batters and Reshaped tab together
+echo Running Script 3...
+python concat.py %1
+
+REM Run Script 4: Fills the gaps among players
+echo Running Script 4...
+python gap_filler.py %1
+
+REM Run Script 5: Fills the gaps in the very beginning if necessary
+echo Running Script 5...
+python gap_start_filler.py %1
+
+REM Run Script 6: Brings all things together
+echo Running Script 6...
+python happy_ever_after.py %1
+
