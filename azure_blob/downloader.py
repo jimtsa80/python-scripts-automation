da15554bd95e35c09f96fd136976cc4d46dde6b5
Dimitris Tsarouhas <dtsarouhas@omilia.com>
2024-05-14 20:42:54 +0300

    blob downloads

--- azure_blob/downloader.py
+++ azure_blob/downloader.py
@@ -1,16 +1,44 @@
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import ContainerClient
+import argparse
+import time
 
-def download_blob(storage_connection_string, container_name, blob_name):
-    blob_service_client = BlobServiceClient.from_connection_string(storage_connection_string)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-    
-    with open(blob_name, "wb") as file:
-        file.write(blob_client.download_blob().readall())
+def download_blobs(container_name, credential):
+    cc = ContainerClient(account_url="https://isieve.blob.core.windows.net", container_name=container_name, credential=credential)
 
-# Set the connection string, container name, blob name
-storage_connection_string = "isieve.blob.core.windows.net"
-container_name = "test" # container in which file is being stored
-blob_name = "000004.jpg" # file which you need to download
+    start_time = time.time()
 
-# Download the blob
-download_blob(storage_connection_string, container_name, blob_name)
+    try:
+        # Example - Download all blobs in the container:
+        for blob in cc.list_blobs():
+            blob_name = blob.name
+            with open(blob_name, "wb") as my_blob:
+                download_blob = cc.download_blob(blob_name)
+                download_blob.readinto(my_blob)
+    except Exception as e:
+        print("Error:", e)
+        return
+
+    end_time = time.time()
+
+    total_time_seconds = end_time - start_time
+    total_time_minutes = total_time_seconds // 60
+    total_time_seconds %= 60
+
+    print("Total time taken for download: {:.0f} minutes {:.2f} seconds".format(total_time_minutes, total_time_seconds))
+
+def main():
+    parser = argparse.ArgumentParser(description='Download blobs from Azure Blob Storage container')
+    parser.add_argument('container_name', type=str, help='Name of the Azure Blob Storage container')
+    args = parser.parse_args()
+
+    try:
+        with open('creds.txt', 'r') as file:
+            credential = file.read().strip()
+    except Exception as e:
+        print("Error reading credentials file:", e)
+        return
+
+    download_blobs(args.container_name, credential)
+
+if __name__ == "__main__":
+    main()
