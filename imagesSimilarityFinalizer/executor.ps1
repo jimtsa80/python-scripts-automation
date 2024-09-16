1f931fb44b6e8adcf56891fab91cbcc9dec42cbc
unknown <jimtsarouhas@gmail.com>
2024-09-17 00:57:36 +0300

    last

--- imagesSimilarityFinalizer/executor.ps1
+++ imagesSimilarityFinalizer/executor.ps1
@@ -1,4 +1,4 @@
 #powershell -ExecutionPolicy Bypass -File .\executor.ps1
 
-powershell -ExecutionPolicy Bypass -File .\run.ps1 -filePath ".\20240818_DETvsNYY_ESPN_185000.xlsx" -sequencesInfoPath "C:\Users\jimtsa80\Desktop\python-scripts-automation\imagesSimilarity\toBeFinalized\20240818_DETvsNYY_ESPN_185000\sequences_info.xlsx"
-powershell -ExecutionPolicy Bypass -File .\run.ps1 -filePath ".\HP_20240818_DETvsNYY_ESPN_185000.xlsx" -sequencesInfoPath "C:\Users\jimtsa80\Desktop\python-scripts-automation\imagesSimilarity\toBeFinalized\20240818_DETvsNYY_ESPN_185000\sequences_info.xlsx"
+powershell -ExecutionPolicy Bypass -File .\run.ps1 -filePath ".\20240901_ARIvsLAD_ROKU_161000.xlsx" -sequencesInfoPath "C:\Users\jimtsa80\Desktop\python-scripts-automation\imagesSimilarity\toBeFinalized\20240901_ARIvsLAD_ROKU_161000\sequences_info.xlsx"
+powershell -ExecutionPolicy Bypass -File .\run.ps1 -filePath ".\HP_20240901_ARIvsLAD_ROKU_161000.xlsx" -sequencesInfoPath "C:\Users\jimtsa80\Desktop\python-scripts-automation\imagesSimilarity\toBeFinalized\20240901_ARIvsLAD_ROKU_161000\sequences_info.xlsx"
